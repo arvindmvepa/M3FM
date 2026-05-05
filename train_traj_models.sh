86404e0c64fedd58e36845c8c338c733d586b0b3
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2026-05-04 23:27:25 -0700

    change order of traj models

--- train_traj_models.sh
+++ train_traj_models.sh
@@ -2,6 +2,6 @@
 
 CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --model_type autoreg_mlp
 CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --model_type residual_mlp
+CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --model_type transformer
 CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --model_type gru
 CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --model_type lowrank_linear
-CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --model_type transformer
