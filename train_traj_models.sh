0130dd9278ad1d0ca53b9871c7513f743c5c7ad2
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2026-05-04 23:16:32 -0700

    add traj model script

---
+++ train_traj_models.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --model_type autoreg_mlp
+CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --model_type residual_mlp
+CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --model_type gru
+CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --model_type lowrank_linear
+CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --model_type transformer
