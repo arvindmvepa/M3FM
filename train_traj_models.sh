459aca8ab91313242f54314f0a94ddf93b720542
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2026-05-06 18:43:08 -0700

    add aux training for longitudinal encoders

--- train_traj_models.sh
+++ train_traj_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --base_loss huber --model_type autoreg_mlp
-CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --base_loss huber --model_type residual_mlp
-CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --base_loss huber --model_type transformer
-CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --base_loss huber --model_type gru
-CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --base_loss huber --model_type lowrank_linear
+CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --base_loss huber --model_type autoreg_mlp --aux_mode joint --aux_style attached --aux_loss_weight 0.1
+CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --base_loss huber --model_type residual_mlp --aux_mode joint --aux_style attached --aux_loss_weight 0.1
+CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --base_loss huber --model_type transformer --aux_mode joint --aux_style attached --aux_loss_weight 0.1
+CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --base_loss huber --model_type gru --aux_mode joint --aux_style attached --aux_loss_weight 0.1
+CUDA_VISIBLE_DEVICES=$1 python train_embedding_trajectory.py --base_loss huber --model_type lowrank_linear --aux_mode joint --aux_style attached --aux_loss_weight 0.1
